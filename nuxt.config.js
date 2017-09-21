f2dfe31947b3de2e6a20806d083ef8f0e3a99ed1
VioletQQY <forviolet771@gmail.com>
2017-09-21 16:35:10 +0800

    base url config

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,6 +13,9 @@ module.exports = {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
+  router: {
+    base: '/healthcloudcp-app-h5/'
+  },
   /*
   ** Customize the progress-bar color
   */
