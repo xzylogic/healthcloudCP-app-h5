072e26d88672fc93f59a7237515738c723b7db10
VioletQQY <forviolet771@gmail.com>
2017-09-27 14:55:50 +0800

    add agreement

---
+++ pages/protocol/agreement.vue
@@ -0,0 +1,547 @@
+<template>
+  <div id="agreement" v-title="'用户协议'" class="container">
+    <h1 class="title">“北京昌平健康云管理平台”用户服务协议</h1>
+    <p class="p2">
+      <span class="s3">
+        <strong>1.0 </strong>
+      </span>
+      <span class="s2">
+        <strong>协议导读及提示：</strong>
+      </span>
+    </p>
+    <p class="p2">
+      <span class="s3">1.1 </span>
+      <span class="s2">欢迎使用北京昌平健康云管理平台（以下简称</span>
+      <span class="s3">“</span>
+      <span class="s2">昌平健康云</span>
+      <span class="s3">”</span>
+      <span class="s2">）。依据本协议的规定为昌平健康云的个人用户（以下简称</span>
+      <span class="s3">“</span>
+      <span class="s2">用户</span>
+      <span class="s3">”</span>
+      <span class="s2">或</span>
+      <span class="s3">“</span>
+      <span class="s2">您</span>
+      <span class="s3">”</span>
+      <span class="s2">）提供服务。本协议在用户和北京市昌平区卫生和计划生育委员会（以下简称</span>
+      <span class="s3">“</span>
+      <span class="s2">昌平区卫生计生委</span>
+      <span class="s3">”</span>
+      <span class="s4">）之间具有合同上的法律效力。特别提醒您审慎阅读、充分理解本协议各条款，特别是其中所涉及的免责以及对用户权利限制条款等。</span>
+      <span class="s2">
+        <strong>限制、免责条款可能以黑体加粗、高亮颜色区分或加下划线的形式提示您着重关注。</strong>
+      </span>
+    </p>
+    <p class="p2">
+      <span class="s3">1.2 </span>
+      <span class="s2">本协议是您与昌平区卫生计生委就昌平健康云服务等相关事宜所订立的契约。除非您已阅读并接受本协议所有条款，
+        <strong>在点击</strong>
+      </span>
+      <span class="s3">
+        <strong>&quot;</strong>
+      </span>
+      <span class="s2">
+        <strong>同意</strong>
+      </span>
+      <span class="s3">
+        <strong>&quot;</strong>
+      </span>
+      <span class="s2">
+        <strong>按钮后，本协议即构成对双方共具约束的法律文件</strong>。您对本平台的登录、查看、发布信息等使用行为，即视为您已阅读并同意本协议的约束。</span>
+    </p>
+    <p class="p2">
+      <span class="s3">1.3 </span>
+      <span class="s2">
+        <strong>如果您仍未满</strong>
+      </span>
+      <span class="s3">
+        <strong>18</strong>
+      </span>
+      <span class="s2">
+        <strong>周岁，请在法定监护人的陪同下阅读本协议，并特别注意未成年人使用条款。</strong>
+      </span>
+    </p>
+    <p class="p2">
+      <span class="s3">1.4 </span>
+      <span class="s2">昌平区卫生计生委有权对本协议条款进行修订、更新。修订、更新后的协议一旦公布即有效代替原来的协议，即时您可随时查阅最新的协议内容。</span>
+    </p>
+    <p class="p2">
+      <span class="s3">
+        <strong>2.0 </strong>
+      </span>
+      <span class="s2">
+        <strong>平台服务声明</strong>
+      </span>
+    </p>
+    <p class="p2">
+      <span class="s3">2.1 </span>
+      <span class="s2">昌平健康云运用万达信息研发的系统，通过互联网等方式为您提供与医疗相关的预约、随访、咨询、资讯等网络服务。</span>
+    </p>
+    <p class="p2">
+      <span class="s3">2.2 </span>
+      <span class="s2">昌平区卫生计生委对于您的通知及其他任何协议、公告或其他关于用户使用平台账号及服务的通知，您同意昌平区卫生计生委可通过昌平健康云公告、电子邮件、手机短信、无线通讯装置等电子方式或常规的信件传递等方式进行，通知于发送之日视为已送达收件人。因信息传输等原因导致您未在前述通知发出当日收到通知的，昌平区卫生计生委不承担责任。</span>
+    </p>
+    <p class="p2">
+      <span class="s3">2.3 </span>
+      <span class="s2">
+        <strong>您必须自行准备如下设备和承担如下开支：（</strong>
+      </span>
+      <span class="s3">
+        <strong>1</strong>
+      </span>
+      <span class="s2">
+        <strong>）上网设备，包括并不限于电脑或者其他上网终端、调制解调器及其他上网装置；（</strong>
+      </span>
+      <span class="s3">
+        <strong>2</strong>
+      </span>
+      <span class="s2">
+        <strong>）上网开支，包括并不限于网络接入费、上网设备租用费、手机流量费等。</strong>
+      </span>
+    </p>
+    <p class="p2">
+      <span class="s3">2.4 </span>
+      <span class="s2">昌平区卫生计生委保留随时修改或中断服务而不需通知您的权利。昌平区卫生计生委有权行使修改或中断服务的权利，不需对您或任何无直接关系的第三方负责。</span>
+    </p>
+    <p class="p3">
+      <span class="s3">2.5 </span>
+      <span class="s2">昌平区卫生计生委
+        <strong>可以依其对判断暂时停止提供、限制或改变昌平健康云的服务，只要您仍然使用昌平健康云服务，即表示您仍然同意本服务协议。</strong>
+      </span>
+    </p>
+    <p class="p2">
+      <span class="s3">
+        <strong>3.0 </strong>
+      </span>
+      <span class="s2">
+        <strong>用户服务使用规范</strong>
+      </span>
+    </p>
+    <p class="p2">
+      <span class="s3">3.1 </span>
+      <span class="s2">此项所述</span>
+      <span class="s3">“</span>
+      <span class="s2">服务使用</span>
+      <span class="s3">”</span>
+      <span class="s2">是指您使用本服务所进行的任何行为，包括但不限于注册、登录、申请认证等行为。</span>
+    </p>
+    <p class="p2">
+      <span class="s3">3.2 </span>
+      <span class="s2">您须对在昌平健康云上的注册信息的真实性、合法性、有效性承担全部责任。您需使用本人身份证完成实名制注册，且不得冒充他人、不得利用他人的名义发布任何信息、不得恶意使用注册账号导致他人误认，否则昌平区卫生计生委有权立即停止提供服务，回收昌平健康云帐号并由您独自承担由此而产生的一切法律责任。</span>
+    </p>
+    <p class="p2">
+      <span class="s3">3.3 </span>
+      <span class="s2">您充分了解并同意，昌平健康云仅是作为为您分享、传播及获取信息的平台。您必须为自己注册账号下的一切行为负责，包括您所发表的任何内容以及由此产生的任何后果。您应对昌平健康云中发布的讯息自行加以判断，并承担因使用内容而引起的所有风险，包括因对内容的正确性、完整性或实用性的依赖而产生的风险。昌平区卫生计生委无法且不会对因前述风险而导致的任何损失或损害承担责任。</span>
+    </p>
+    <p class="p2">
+      <span class="s3">3.4 </span>
+      <span class="s2">您理解并同意，昌平区卫生计生委一直致力于为用户提供文明健康、规范有序的网络环境，您被禁止利用本平台制作、复制、发布、传播如下干扰昌平健康云正常运营，以及侵犯其他用户或第三方合法权益的内容：</span>
+      <span class="s3">
+        <span class="Apple-converted-space">&nbsp;</span>
+      </span>
+    </p>
+    <p class="p4">
+      <span class="s3">3.4.1 </span>
+      <span class="s2">
+        <strong>禁止发布、传送、传播、储存违反国家法律法规的内容，包括但不限于：</strong>
+      </span>
+    </p>
+    <ol class="ol1">
+      <ul class="ul1">
+        <li class="li2">
+          <span class="s5"></span>
+          <span class="s2">违反国家宪法制定的基本原则；</span>
+        </li>
+        <li class="li2">
+          <span class="s5"></span>
+          <span class="s2">危害国家安全，泄露国家秘密，颠覆国家政权，破坏国家统一；</span>
+        </li>
+        <li class="li2">
+          <span class="s5"></span>
+          <span class="s2">损害国家荣誉和利益；</span>
+        </li>
+        <li class="li2">
+          <span class="s5"></span>
+          <span class="s2">煽动民族仇恨、民族歧视，破坏民族团结；</span>
+        </li>
+        <li class="li2">
+          <span class="s5"></span>
+          <span class="s2">破坏国家宗教政策，宣扬邪教和封建迷信；</span>
+        </li>
+        <li class="li2">
+          <span class="s5"></span>
+          <span class="s2">散布谣言，扰乱社会秩序，破坏社会稳定；</span>
+        </li>
+        <li class="li2">
+          <span class="s5"></span>
+          <span class="s2">散布淫秽、色情、赌博、暴力、恐怖或者教唆犯罪；</span>
+        </li>
+        <li class="li2">
+          <span class="s5"></span>
+          <span class="s2">侮辱或者诽谤他人，侵害他人合法权益；</span>
+        </li>
+        <li class="li2">
+          <span class="s5"></span>
+          <span class="s2">煽</span>
+          <span class="s3"> </span>
+          <span class="s2">动非法集会、结社、游行、示威、聚众扰乱社会秩序；</span>
+        </li>
+        <li class="li2">
+          <span class="s5"></span>
+          <span class="s2">以非法民间组织名义活动；</span>
+        </li>
+        <li class="li2">
+          <span class="s5"></span>
+          <span class="s2">含有国家或当地政府法律、行政法规禁止的其他内容。</span>
+        </li>
+      </ul>
+    </ol>
+    <p class="p4">
+      <span class="s3">3.4.2 </span>
+      <span class="s2">禁止发布、传输、传播、储存侵害他人名誉权、肖像权、知识产权、商业秘密等合法权利的内容；</span>
+    </p>
+    <p class="p4">
+      <span class="s3">3.4.3 </span>
+      <span class="s2">禁止涉及他人隐私、个人信息或资料；</span>
+    </p>
+    <p class="p4">
+      <span class="s3">3.4.4 </span>
+      <span class="s2">禁止发布、传输、传播骚扰信息、广告信息及垃圾信息或含有任何性暗示的文字或图片；</span>
+    </p>
+    <p class="p4">
+      <span class="s3">3.4.5 </span>
+      <span class="s2">禁止发表、传输任何违法犯罪的、骚扰性的、中伤他人的、辱骂性的、恐吓性的、伤害性的、庸俗的，淫秽的、不文明的等信息资料；</span>
+    </p>
+    <p class="p4">
+      <span class="s3">3.4.6 </span>
+      <span class="s2">其他违反法律法规、政策及公序良俗、社会公德或干扰昌平健康云正常运营和侵犯其他用户或第三方合法权益内容的信息。</span>
+    </p>
+    <p class="p2">
+      <span class="s3">3.5 </span>
+      <span class="s2">除本协议或另有约定外，您理解并保证不利用在昌平健康云注册的账号进行经营活动、牟利行为及其他未经昌平健康云许可的行为，包括但不仅限于以下行为：</span>
+      <span class="s3">
+        <span class="Apple-converted-space">&nbsp;</span>
+      </span>
+    </p>
+    <p class="p4">
+      <span class="s3">3.5.1 </span>
+      <span class="s2">您账号内的任何昌平健康云优惠信息由昌平区卫生计生委享有解释权和修改权，您仅享有在昌平健康云上的使用权，严禁转卖昌平健康云账号、积分或其他类型的优惠券、礼品卡、或利用昌平健康云账号进行其他经营性行为等；</span>
+    </p>
+    <p class="p4">
+      <span class="s3">3.5.2 </span>
+      <span class="s2">恶意利用技术手段或其他方式，为获取优惠、折扣或其他利益而注册多个账号等行为，影响其他用户正常消费行为或相关合法权益、影响昌平健康云正常秩序的行为；</span>
+    </p>
+    <p class="p4">
+      <span class="s3">3.5.3 </span>
+      <span class="s2">以再销售或商业使用为目的的，对昌平健康云服务进行的购买行为（与昌平区卫生计生委另有合同约定的除外）；</span>
+    </p>
+    <p class="p4">
+      <span class="s3">3.5.4 </span>
+      <span class="s2">其他影响昌平健康云对用户账号正常管理秩序的行为。</span>
+    </p>
+    <p class="p2">
+      <span class="s3">
+        <strong>4.0 </strong>
+      </span>
+      <span class="s2">
+        <strong>用户账户管理及安全</strong>
+      </span>
+    </p>
+    <p class="p2">
+      <span class="s3">4.1 </span>
+      <span class="s2">当您遵照并同意本协议内容，成功申请注册后，昌平健康云会为您开通一个个人用户账号，作为您使用昌平健康云服务的唯一身份标识。
+        <strong>该账号的所有权归属于昌平区卫生计生委，您仅拥有使用权，帐号使用权禁止赠与、借用、租用、转让或售卖。您在注册账号时，必须依据昌平区卫生计生委所制定的要求，填写您的有效身份证号并上传身份验证资料进行用户实名制身份验证。昌平区卫生计生委将根据您所提供的资料进行身份审核，审核通过后将为您开通账号。</strong>
+      </span>
+    </p>
+    <p class="p2">
+      <span class="s3">4.2 </span>
+      <span class="s2">
+        <strong>登录账号时，您必须使用已通过实名制身份验证的昌平健康云账号进行登录。</strong>在登录时您必须输入您设定并保管的账号密码。为保护您的权益，您在设定账号的密码时，请勿使用重复性或者连续数字的简单密码。
+        <strong>请您对密码加以妥善保管，方法包括但不限于：切勿将密码告知除您以外的第二人，安装防病毒木马软件、定期更改密码等措施。因您密码保管不善而造成的所有损失由您自行承担。</strong>
+      </span>
+    </p>
+    <p class="p2">
+      <span class="s3">4.3 </span>
+      <span class="s2">
+        <strong>您对通过您的账号所进行的活动、行为、言论和事件依法享有权利和承担责任。且不得以任何形式转让、授权除您以外的第二人使用以您姓名和证件号进行实名制注册的账号以及与他人交易账号。</strong>
+      </span>
+    </p>
+    <p class="p2">
+      <span class="s3">4.4 </span>
+      <span class="s2">鉴于互联网服务的特殊性，昌平区卫生计生委无义务审核是否是您本人使用该组用户名及密码，仅审核用户名及密码是否与数据库中保存的信息一致，任何人只要输入的用户名及密码与数据库中保存的一致，即
+        <strong>可凭借该组用户名及密码登陆并获得昌平健康云所提供的各类服务，所以即使您认为您的账号登陆行为并非您本人所为，昌平区卫生计生委将不承担因此而产生的任何责任。</strong>
+      </span>
+    </p>
+    <p class="p2">
+      <span class="s3">4.5 </span>
+      <span class="s2">您可以为您的账号设定昵称，即：账号用户名称，但该昵称不得侵犯他人合法权益。
+        <strong>如昌平区卫生计生委认为您设定的昵称涉嫌侵犯他人合法权益的，昌平区卫生计生委有权终止为您提供服务，并注销您的账号和昵称。</strong>若您不再是昌平健康云的注册用户，对于您的原注册账号的昵称，昌平区卫生计生委可以将其开放给任意注册用户。</span>
+    </p>
+    <p class="p2">
+      <span class="s3">4.6 </span>
+      <span class="s2">
+        <strong>如您违反相关法律法规、本协议以及专项规则的规定，昌平区卫生计生委有权进行独立判断并随时限制、冻结或终止您对昌平健康云帐号的使用，并根据您申诉提交材料的实际情况决定是否为您恢复使用。对此给您带来的损失（包括但不限于通信中断，用户资料及相关数据清空等），由您自行承担。</strong>
+      </span>
+    </p>
+    <p class="p2">
+      <span class="s3">4.7 </span>
+      <span class="s2">
+        <strong>您理解并同意，在您未进行举报或提出帐号申诉等方式明确告知昌平健康云帐号被他人使用或信息数据泄漏等情况并提供相关证明材料前，昌平区卫生计生委有理由相信该帐号行为是您使用帐号的行为。</strong>
+      </span>
+    </p>
+    <p class="p2">
+      <span class="s3">4.8 </span>
+      <span class="s2">
+        <strong>您不得通过任何手段恶意注册昌平健康云帐号，包括但不限于以牟利、炒作、套现等为目的多个账号注册。</strong>
+      </span>
+    </p>
+    <p class="p2">
+      <span class="s3">4.9 </span>
+      <span class="s2">
+        <strong>为了充分利用资源，如果您的账号在连续</strong>
+      </span>
+      <span class="s3">
+        <strong>720</strong>
+      </span>
+      <span class="s2">
+        <strong>天内未登录使用的情形，昌平区卫生计生委权清理该帐号，停止账号的使用权。</strong>
+      </span>
+    </p>
+    <p class="p2">
+      <span class="s3">
+        <strong>5.0 </strong>
+      </span>
+      <span class="s2">
+        <strong>用户信息的收集和管理</strong>
+      </span>
+    </p>
+    <p class="p2">
+      <span class="s3">5.1 </span>
+      <span class="s2">
+        <strong>您承诺以您的真实身份信息注册昌平健康云，并确认为此所预留的个人信息为真实、准确、完整、合法、有效，昌平区卫生计生委可以通过您预留的个人信息联系到您本人。请务必在您的个人信息发生变更前及时自行登录昌平健康云同步更新，并依照法律规定和本协议约定您对您预留的信息承担相应的法律责任。</strong>昌平区卫生计生委有权审查您在注册所预留个人信息是否真实、有效。若这些信息与您当前实际情况不符或有误导之嫌，致使昌平区卫生计生委无法为您提供服务或进一步的联系到您本人，昌平区卫生计生委将不会因此承担任何责任，由此引起的相应责任及后果将由您自行承担，并且昌平区卫生计生委保留回收您对账号的使用权或并就此注销，终止使用昌平健康云各项服务的权利。</span>
+    </p>
+    <p class="p2">
+      <span class="s3">5.2 </span>
+      <span class="s2">您在使用昌平健康云各项服务时，如有涉及您的真实姓名、名称、通讯方式、联系地址、健康指数等私人信息的，昌平区卫生计生委将予以严格保密，除非得到您的授权或法律另有规定，昌平区卫生计生委将不会向外界披露，但以下情况除外：</span>
+      <span class="s3">
+        <span class="Apple-converted-space">&nbsp;</span>
+      </span>
+    </p>
+    <ul class="ul1">
+      <li class="li2">
+        <span class="s6"></span>
+        <span class="s2">提供独立服务且仅需要与其服务相关信息的单位，如社区卫生服务中心等；</span>
+      </li>
+      <li class="li2">
+        <span class="s6"></span>
+        <span class="s2">具有合法调阅信息权限并从合法渠道调阅信息的政府部门或其他机构，如公安机关、法院；</span>
+      </li>
+      <li class="li2">
+        <span class="s6"></span>
+        <span class="s2">经您或您授权同意的第三方单位。</span>
+      </li>
+    </ul>
+    <p class="p2">
+      <span class="s3">5.3 </span>
+      <span class="s2">昌平区卫生计生委依照您在昌平健康云上的行为操作，由系统自动统计、计算关于您某些行为的信息。在保证不向外界透露您的私人信息的前提下，昌平区卫生计生委有权对整个用户数据库进行分析并对用户数据库进行卫生行政管理服务方面的利用。</span>
+    </p>
+    <p class="p2">
+      <span class="s3">
+        <strong>7.0 </strong>
+      </span>
+      <span class="s2">
+        <strong>知识产权保护</strong>
+      </span>
+    </p>
+    <p class="p2">
+      <span class="s3">7.1 </span>
+      <span class="s2">您在昌平健康云上发表的文章及图片（包括转贴的文章及图片）版权仅归属原作者所有，若作者有版权声明或原作从其他网站转载而附带有原版权声明者，其版权归属以附带声明为准。</span>
+    </p>
+    <p class="p2">
+      <span class="s3">7.2 </span>
+      <span class="s2">
+        <strong>当您转载、引用任何发表于昌平健康云上的版权文章须符合以下规范：</strong>
+      </span>
+    </p>
+    <ul class="ul1">
+      <li class="li2">
+        <span class="s6"></span>
+        <span class="s2">用于非商业、非盈利、非广告性目的时需注明作者及文章及图片的出处，并说明用于</span>
+        <span class="s3">“</span>
+        <span class="s2">非商业、非盈利、非广告性</span>
+        <span class="s3">”</span>
+        <span class="s2">；</span>
+      </li>
+      <li class="li2">
+        <span class="s6"></span>
+        <span class="s2">用于商业、盈利、广告性目的时需征得文章或图片原作者的同意，并与作者或版权所有人协商费用问题达成一致意见，注明作者姓名、授权范围及原作出处；</span>
+      </li>
+      <li class="li2">
+        <span class="s6"></span>
+        <span class="s2">任何文章或图片的修改或删除均应保持作者原意并征求原作者同意，与作者或版权所有人协商费用问题达成一致意见，注明授权范围。</span>
+      </li>
+    </ul>
+    <p class="p2">
+      <span class="s3">7.3 </span>
+      <span class="s2">
+        <strong>您在昌平健康云发表的文章仅代表您个人的观点，与昌平区卫生计生委立场无关。为此所产生的一切后果由您自负。</strong>
+      </span>
+    </p>
+    <p class="p2">
+      <span class="s3">7.4 </span>
+      <span class="s2">据《中华人民共和国侵权责任法》第三十六条，如任何单位或个人提出，您利用昌平健康云侵害其本人的民事权益或实施侵权行为的，包括但不限于侮辱、诽谤等，被侵权人有权书面通知昌平区卫生计生委采取删除、屏蔽、断开链接等措施。</span>
+    </p>
+    <p class="p2">
+      <span class="s3">7.5 </span>
+      <span class="s2">据《信息网络传播权保护条例》，任何单位或个人提出，昌平健康云所涉及的文档、影音等作品、，侵犯了其本人的信息网络传播权或者被删除、改变了其本人权利所管理电子信息的，可以向昌平区卫生计生委提交书面通知，要求昌平健康云删除该侵权作品，或者断开链接。</span>
+    </p>
+    <p class="p2">
+      <span class="s3">7.6 </span>
+      <span class="s2">
+        <strong>如任何单位或个人提出昌平健康云用户发布的任何信息侵犯其合法权益的，包括但不限于以上两点，在有充分法律法规及证据足以证明的情况下，均可以通过下列联系方式通知昌平区卫生计生委：</strong>
+      </span>
+    </p>
+    <ul class="ul1">
+      <li class="li5">
+        <span class="s6"></span>
+        <span class="s7">通讯邮箱：</span>
+        <span class="s2"> <br/>2727335513@qq.com</span>
+      </li>
+      <li class="li5">
+        <span class="s6"></span>
+        <span class="s7">客服电话：</span>
+        <span class="s2"> <br/>010-69746223</span>
+      </li>
+    </ul>
+    <p class="p2">
+      <span class="s3">
+        <strong>8.0 </strong>
+      </span>
+      <span class="s2">
+        <strong>拒绝担保与免责</strong>
+      </span>
+    </p>
+    <p class="p2">
+      <span class="s3">8.1 </span>
+      <span class="s2">
+        <strong>昌平区卫生计生委是</strong>
+      </span>
+      <span class="s3">
+        <strong>“</strong>
+      </span>
+      <span class="s2">
+        <strong>互联网服务</strong>
+      </span>
+      <span class="s3">
+        <strong>”</strong>
+      </span>
+      <span class="s2">
+        <strong>的第三方平台，不确保平台上的信息及服务能充分满足您的需求。对于您在接受昌平区卫生计生委的服务过程中可能遇到的错误、侮辱、诽谤、不作为、淫秽、色情或亵渎事件，昌平区卫生计生委不承担法律责任。</strong>
+      </span>
+    </p>
+    <p class="p2">
+      <span class="s3">8.2 </span>
+      <span class="s2">由于互联网传输存在其特殊性，昌平区卫生计生委不担保不会受中断，对服务的及时性、安全性都不作担保，不承担非因昌平区卫生计生委导致的责任。</span>
+    </p>
+    <p class="p2">
+      <span class="s3">8.3 </span>
+      <span class="s2">昌平区卫生计生委致力于使您能安全访问和使用昌平健康云平台，但昌平区卫生计生委不声明也不保证昌平健康云平台或其服务器是不含病毒或其它潜在有害因素的；因此您应使用业界公认的软件查杀任何自昌平健康云平台所下载的文件中的病毒。</span>
+    </p>
+    <p class="p2">
+      <span class="s3">8.4 </span>
+      <span class="s2">昌平区卫生计生委有权删除昌平健康云上各类不符合法律或协议规定的评论或讯息，而保留不通知您的权利。</span>
+    </p>
+    <p class="p2">
+      <span class="s3">8.5 </span>
+      <span class="s2">
+        <strong>所有发给您的通告，昌平区卫生计生委都将通过正式的页面公告、站内信、电子邮件、客服电话、手机短信或常规的信件送达。任何未经昌平区卫生计生委正规渠道获得的中奖、优惠等活动或信息，昌平区卫生计生委不承担法律责任。</strong>
+      </span>
+    </p>
+    <p class="p2">
+      <span class="s3">8.6 </span>
+      <span class="s2">昌平健康云致力于提供正确、完整的健康资讯，但不保证信息的正确性和完整性，且不对因信息的不正确或遗漏导致的任何损失或损害承担责任。</span>
+    </p>
+    <p class="p5">
+      <span class="s2">
+        <strong>9.0 </strong>
+      </span>
+      <span class="s7">
+        <strong>法律责任</strong>
+      </span>
+    </p>
+    <p class="p2">
+      <span class="s3">9.1 </span>
+      <span class="s2">
+        <strong>本协议是由您与昌平区卫生计生委共同签订的，适用于您在昌平区卫生计生委的全部行为或活动。协议内容包括但不限于：本协议正文及已经发布的或将来可能发布的各类规则，所有条款和规则均是本协议不可分割的部分，与本协议正文具有相同的法律效力。</strong>
+      </span>
+    </p>
+    <p class="p2">
+      <span class="s3">9.2 </span>
+      <span class="s2">如本协议中的任何条款如被视为无效或不具有执行力，则应认为该条款可与本协议相分割。在此情况下，不影响本协议的其他条款有效性和法律效应。</span>
+    </p>
+    <p class="p2">
+      <span class="s3">9.3 </span>
+      <span class="s2">
+        <strong>本协议是昌平区卫生计生委与已注册成为昌平健康云用户之间重要的法律文件，任何其他书面或者口头意思所表示含义与本协议不一致的，均应当以本协议为准。</strong>
+      </span>
+    </p>
+    <p class="p2">
+      <span class="s3">9.4 </span>
+      <span class="s2">昌平区卫生计生委对本服务协议拥有最终的解释权。</span>
+    </p>
+    <p class="p2">
+      <span class="s3">9.5 </span>
+      <span class="s2">本协议的订立、执行和解释及争议的解决均应适用中华人民共和国的法律。如就本协议内容或执行发生任何争议，双方应首先通过友好协商方式解决；协商不成的，任何一方应向昌平区卫生计生委所在地的人民法院提起诉讼。</span>
+    </p>
+    <p class="p2">
+      <span class="s3">9.6 </span>
+      <span class="s2">本协议中的各类标题仅为方便阅读而设，本身不具有任何含义，不作为解释本协议的依据。</span>
+    </p>
+    <p class="p6" style="text-align: right;padding-top: 16px">
+      <span class="s2">北京市昌平区卫生和计划生育委员会</span>
+    </p>
+    <p class="p5" style="text-align: right">
+      <span class="s2">
+        <strong>2017</strong>
+      </span>
+      <span class="s7">
+        <strong>年</strong>
+      </span>
+      <span class="s2">
+        <strong>09</strong>
+      </span>
+      <span class="s7">
+        <strong>月更新</strong>
+      </span>
+    </p>
+    <p>
+      <br/>
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'agreement'
+  }
+</script>
+
+<style>
+  .container {
+    padding: 32px 15px;
+    line-height: 1.5;
+    font-size: 16px;
+    color: #333;
+  }
+
+  .container .title {
+    font-size: 18px;
+    font-weight: 700;
+    text-align: center;
+    padding: 0 8px 16px;
+  }
+
+  .container .p2 {
+    padding-bottom: 10px;
+  }
+</style>
