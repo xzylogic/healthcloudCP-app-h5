24f174d68272ec50514a6c486a0e1f226406712f
VioletQQY <forviolet771@gmail.com>
2017-10-17 14:30:36 +0800

    add article loading

--- pages/article/_id.vue
+++ pages/article/_id.vue
@@ -1,9 +1,20 @@
 <template>
   <div id="article" v-title="' '">
-    <div class="container">
+    <div v-show="!content&&!errMsg">
+      <div class="error-content">
+        <img src="~/assets/loading.gif" alt="Loading..." width="200">
+      </div>
+    </div>
+    <div class="container" v-show="content">
       <h1 class="title">{{title}}</h1>
       <p class="date">{{date}}</p>
       <div v-html="content"></div>
+    </div>
+    <div v-show="!content&&errMsg">
+      <div class="error-content">
+        <img src="~/assets/neterror.png" alt="NETWORK ERROR" width="100">
+        <p>暂无网络</p>
+      </div>
     </div>
   </div>
 </template>
@@ -17,17 +28,27 @@
       return {
         title: '',
         date: '',
-        content: ''
+        content: '',
+        errMsg: ''
       }
     },
     created () {
       this.$http.get(`${this.$baseUrl}/api/article/info?id=${this.$route.params.id}&source=h5`)
         .then(res => {
           if (res && res.data && res.data.code === 0 && res.data.data) {
+            this.errMsg = ''
             this.title = res.data.data.title || ''
             this.date = moment(res.data.data.updateTime || new Date()).format('YYYY-MM-DD HH:mm')
             this.content = res.data.data.content || ''
+          } else {
+            console.log(res)
+            this.content = ''
+            this.errMsg = '啊哦～请求失败！'
           }
+        }, err => {
+          console.log(err)
+          this.content = ''
+          this.errMsg = '啊哦～请求失败！'
         })
     }
   }
@@ -52,5 +73,13 @@
     font-size: 11px;
     font-weight: 400;
     color: #999;
+  }
+
+  .error-content {
+    width: 100%;
+    height: 300px;
+    padding-top: 30%;
+    text-align: center;
+    color: #666;
   }
 </style>
