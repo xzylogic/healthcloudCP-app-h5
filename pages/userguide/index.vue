7b8332a4fa0041236d2e6794a5882009c9a442b3
VioletQQY <forviolet771@gmail.com>
2018-12-17 18:49:55 +0800

    add userguide

---
+++ pages/userguide/index.vue
@@ -0,0 +1,282 @@
+<template>
+  <div id="userguide">
+    <div class="wrap">
+      <h1 class="header">昌平健康云App居民端<br/>用户指南v0.1</h1>
+      <div class="sub">2018年12月</div>
+      <ul>
+        <li>
+          <h3>一、注册、登录与实名认证</h3>
+          <ul>
+            <li>
+              <h4>1. 注册帐号</h4>
+              <ul>
+                <li>
+                  <p>1) 若没有帐号，点击【注册帐号】，进入帐号注册页面</p>
+                  <figure class="image">
+                    <img src="~assets/null.png" />
+                  </figure>
+                </li>
+                <li>
+                  <p>2) 填写完注册信息后，点击【注册】，完成帐号注册并自动登入后返回App首页。（若密码设置简单，则会弹出温馨提醒，点击【我就是任性】继续完成注册）</p>
+                  <figure class="image">
+                    <img src="~assets/null.png" />
+                  </figure>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <h4>2. 登录</h4>
+              <ul>
+                <li>
+                  <p>1) 验证码登录：输入手机号，获取验证码，登录</p>
+                  <figure class="image">
+                    <img src="~assets/null.png" />
+                  </figure>
+                </li>
+                <li>
+                  <p>2) 密码登录：输入手机号+密码，登录</p>
+                  <figure class="image">
+                    <img src="~assets/null.png" />
+                  </figure>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <h4>3. 实名认证</h4>
+              <ul>
+                <li>
+                  <p>1) 进入我的模块页面。点击【头像】进入个人资料页面。点击【实名认证】项目，进行实名认证页面</p>
+                  <figure class="image">
+                    <img src="~assets/null.png" />
+                  </figure>
+                </li>
+                <li>
+                  <p>2) 输入【真实姓名、身份证号】，点击【+】号上传个人证件照。点击【申请实名认证】，提交实名申请。</p>
+                  <figure class="image">
+                    <img src="~assets/null.png" />
+                  </figure>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <h3>二、功能介绍</h3>
+          <ul>
+            <li>
+              <h4>1. 叶酸领取预约流程展示</h4>
+              <ul>
+                <li>
+                  <p>1) 选择预约医院</p>
+                  <figure class="image">
+                    <img src="~assets/null.png" />
+                  </figure>
+                </li>
+                <li>
+                  <p>2) 选择预约时间（日期+时段）</p>
+                  <figure class="image">
+                    <img src="~assets/null.png" />
+                  </figure>
+                </li>
+                <li>
+                  <p>3) 选择申请人</p>
+                  <figure class="image">
+                    <img src="~assets/null.png" />
+                  </figure>
+                  <p><strong>注意事项：</strong></p>
+                  <p><strong>*申请人的户籍属于昌平区，申请人户籍地址街道需与申请医院的街道一致；</strong></p>
+                  <p><strong>*申请人的户籍不属于昌平区，申请人现住地址街道需与申请医院的街道一致；</strong></p>
+                  <br />
+                </li>
+                <li>
+                  <p>4) 确认知情同意书</p>
+                  <figure class="image">
+                    <img src="~assets/null.png" />
+                  </figure>
+                </li>
+                <li>
+                  <p>5) 预约信息确认</p>
+                  <figure class="image">
+                    <img src="~assets/null.png" />
+                  </figure>
+                </li>
+                <li>
+                  <p>6) 提示预约成功</p>
+                  <figure class="image">
+                    <img src="~assets/null.png" />
+                  </figure>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <h4>2. 母子建档预约流程展示</h4>
+              <ul>
+                <li>
+                  <p>1) 选择预约医院</p>
+                  <figure class="image">
+                    <img src="~assets/null.png" />
+                  </figure>
+                </li>
+                <li>
+                  <p>2) 选择预约时间（日期+时段）</p>
+                  <figure class="image">
+                    <img src="~assets/null.png" />
+                  </figure>
+                </li>
+                <li>
+                  <p>3) 选择孕妇</p> 
+                  <figure class="image">
+                    <img src="~assets/null.png" />
+                  </figure>
+                  <p><strong>注意事项：</strong></p>
+                  <p><strong>*申请人的户籍属于昌平区，孕妇户籍地址街道需与申请医院的街道一致；</strong></p>
+                  <p><strong>*申请人的户籍不属于昌平区，孕妇现住地址街道需与申请医院的街道一致；</strong></p>
+                  <br />
+                </li>
+                <li>
+                  <p>4) 选择丈夫</p> 
+                  <figure class="image">
+                    <img src="~assets/null.png" />
+                  </figure>
+                </li>
+                <li>
+                  <p>5) 预约信息确认</p> 
+                  <figure class="image">
+                    <img src="~assets/null.png" />
+                  </figure>
+                </li>
+                <li>
+                  <p>6) 提示预约成功</p> 
+                  <figure class="image">
+                    <img src="~assets/null.png" />
+                  </figure>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <h4>3. 预防接种预约流程展示</h4>
+              <ul>
+                <li>
+                  <p>1) 选择预约医院</p>
+                  <figure class="image">
+                    <img src="~assets/null.png" />
+                  </figure>
+                </li>
+                <li>
+                  <p>2) 选择预约时间（日期+时段）</p>
+                  <figure class="image">
+                    <img src="~assets/null.png" />
+                  </figure>
+                </li>
+                <li>
+                  <p>3) 选择宝宝，若无宝宝，可以通过【添加宝宝】进行添加</p>
+                  <figure class="image">
+                    <img src="~assets/null.png" />
+                  </figure>
+                </li>
+                <li>
+                  <p>4) 选择父母信息</p>
+                  <figure class="image">
+                    <img src="~assets/null.png" />
+                  </figure>
+                  <p><strong>注意事项：</strong></p>
+                  <p><strong>*宝宝母亲的现住地址街道需与申请医院的街道一致；</strong></p>
+                  <br />
+                </li>
+                <li>
+                  <p>5) 预约信息确认</p> 
+                  <figure class="image">
+                    <img src="~assets/null.png" />
+                  </figure>
+                </li>
+                <li>
+                  <p>6) 提示预约成功</p> 
+                  <figure class="image">
+                    <img src="~assets/null.png" />
+                  </figure>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'userguide',
+    data () {
+      // return {
+      //   list: [{
+      //     title: '1．注册与登录',
+      //     content: `<p>一、手机方式登录</p><p>通过你的手机号码注册昌平健康云账号。注册中，不可跳过设置密码步骤，注册完即可登录。</p>`,
+      //     open: false
+      //   }]
+      // }
+    },
+    head () {
+      return {
+        title: '昌平健康云App居民端用户指南v0.1'
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  @import '../../assets/css/font-awesome.css';
+  .wrap {
+    width: 100%;
+    height: 100vh;
+    background: #fff;
+    overflow: auto;
+    padding: 15px;
+    ul,
+    li {
+      list-style: none;
+      padding: 0;
+      margin: 0;
+    }
+  }
+
+  .header {
+    font-size: 24px;
+    text-align: center;
+    padding-bottom: 15px;
+    line-height: 1.75;
+  }
+
+  .sub {
+    font-size: 18px;
+    text-align: center;
+    padding-bottom: 30px;
+    font-weight: 400;
+  }
+
+  h3 {
+    font-size: 18px;
+    line-height: 1.75;
+  }
+
+  h4 {
+    font-size: 16px;
+    line-height: 1.75;
+  }
+
+  ul ul {
+    padding-left: 15px !important;
+  }
+
+  p {
+    font-size: 16px;
+    line-height: 1.5;
+  }
+
+  .image {
+    padding: 15px;
+    img {
+      width: 100%;
+    }
+  }
+</style>
